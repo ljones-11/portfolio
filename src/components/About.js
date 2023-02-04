7271b7294fcd9351b1f8f62dd4a5e76773b92785
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-02-04 16:18:27 +0000

    about page complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Footer from './Footer';
 import NavBar from './NavBar';
 
 const About = () => {
 
     return (
-        <section className='hero-container'>
+        <>
+        
+        <section className='about-container'>
                 <div className='hero-info'>
                     <NavBar />
                     <h1>About Me</h1>
@@ -14,7 +17,7 @@ const About = () => {
                             I have spent the last 12 years playing professional rugby. This gave me the opporunity to represent my country 
                             travel the world, and do what I love everyday as a job. I always knew at some point, 
                             this would come to an end and here we are!
-                            I am now eager to transition my skills and experience into a career in tech.<br></br><br></br> 
+                            I am now transitioning with my skills and experience into a career in tech.<br></br><br></br> 
                             My background in Mechanical Engineering, combined with
                             the 16-week Professional Web Development Course with CodeClan I am currently studying
                             has given me experience with a range of full-stack skills.
@@ -47,24 +50,39 @@ const About = () => {
                         </div>
                     </div>
                 <div className='hero-info'>
-                    <h2>CodeClan</h2>
+                    <h2 id="underline">CodeClan</h2>
                     <div className='text-container'>
                         <p>CodeClan is 16 week intensive SQA accredited course teaching the fundamentals of software development
-                        across a range of languages and frameworks, using Agile methodologies, TDD and OOP to build
+                        across a range of languages and frameworks, using Agile methodologies, Test-Driven Development and Object-Oriented Programming to build
                         full-stack applications. Over 800 hours of instructor-led coding, pair programming and group
                         project experience which has significantly improved and expanded my previous programming
                         knowledge.</p>
                     </div>
                     </div>
                     <div className='hero-info'>
-                    <h2>Skills and languages</h2>
+                    <h2 id="underline">Skills and languages</h2>
                     <div className='text-container'>
-                        <b><p>Python • HTML & CSS • PostgreSQL • Javascript • React • Express.js • MongoDB • Javascript
-                        • Spring • Git • Restful Workflow
+                        <b><p>Python • HTML & CSS • PostgreSQL • Javascript • React • Express.js • MongoDB • Java
+                        • Spring • Git
                         </p></b>
+                    </div>
+                    <div className='icon-container'>
+                        <i class="devicon-python-plain-wordmark colored"></i>
+                        <i class="devicon-html5-plain-wordmark colored"></i>
+                        <i class="devicon-css3-plain-wordmark colored"></i>
+                        <i class="devicon-postgresql-plain-wordmark colored"></i>
+                        <i class="devicon-javascript-plain colored"></i>
+                        <i class="devicon-react-original-wordmark colored"></i>
+                        <i class="devicon-express-original-wordmark"></i>
+                        <i class="devicon-mongodb-plain-wordmark colored"></i>
+                        <i class="devicon-java-plain-wordmark colored"></i>
+                        <i class="devicon-spring-plain-wordmark colored"></i>
+                        <i class="devicon-git-plain-wordmark"></i>
                     </div>
                 </div>
             </section>
+            <Footer />
+            </>
     )
 }
 
