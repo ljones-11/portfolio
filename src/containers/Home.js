190f0197d9e136af732354265e33500d4ac8fbf6
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-02-05 14:38:15 +0000

    photos

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,21 @@
 import React, { require } from 'react'
+import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 
 const Home = () => {
     return (
-            
-            <section className='hero-container'>
-                <div className='hero-info'>
-                    <NavBar />
-                    <h1>Lee Jones</h1>
-                    <h2>Software Developer</h2>
-                    <a>Download CV</a>
-                </div>
-            </section>
+            <>
+                <section className='hero-container'>
+                    <div className='hero-info'>
+                        <NavBar />
+                        <h1>Lee Jones</h1>
+                        <hr id="hr-title"></hr>
+                        <h2>Software Developer</h2><br></br>
+                        <a href="mailto:ljones2806@gmail.com">E-mail me</a>
+                    </div>
+                </section>
+                <Footer />
+            </>
     )
 }
 
